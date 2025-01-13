6b7a0b05d4d7bec4a902fa82381d7c5fb93d760c
Shaun Donnelly <shaundonnelly@gmail.com>
2025-01-13 11:47:04 +0000

    slight refactor

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,101 @@
 import { Button, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 import ExpoTmdemo from 'expo-tmdemo';
+import { useEffect, useState } from "react";
 
 const config = require('../config.json');
 
 export default function Index() {
 
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+
+  const initialize = async () => {
+    await ExpoTmdemo.initialize({
+      apiKey: config.apiKey
+    });
+    const didInitialize = await ExpoTmdemo.isInitialized();
+    setIsInitialized(didInitialize);
+  }
+
+  const deinitialize = async () => {
+    await ExpoTmdemo.deinitialize();
+    const didDeinitialize = await ExpoTmdemo.isInitialized();
+    setIsInitialized(didDeinitialize);
+  }
+
+  const startRecording = async () => {
+    await ExpoTmdemo.startRecording();
+    const recording = await ExpoTmdemo.isRecordingInProgress();
+    setIsRecording(recording);
+  }
+
+  const stopRecording = async () => {
+    await ExpoTmdemo.stopRecording();
+    const recording = await ExpoTmdemo.isRecordingInProgress();
+    setIsRecording(recording);
+  }
+
+  useEffect(() => {
+    const initialized = ExpoTmdemo.isInitialized()
+    setIsInitialized(initialized);
+
+    const recording = ExpoTmdemo.isRecordingInProgress()
+    setIsRecording(recording);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
         <Text style={styles.header}>Module API Example</Text>
+
       
         <Group name="Init">
 
-        {
-        (ExpoTmdemo.isInitialized())
-        ? 
-        <>
-        <Button
-            title="Deinitialize"
-            onPress={async () => {
-              await ExpoTmdemo.deinitialize();
-            }}
-          />
-        </>
-        : 
-        <>
-          <Button
-            title="Initialize"
-            onPress={async () => {
-              await ExpoTmdemo.initialize({
-                apiKey: config.apiKey
-              });
-            }}
-          />
-          </>
-        }
+          {isInitialized && (
+            <>
+            <Text>Initialized</Text>
+            <Button
+              title="Deinitialize"
+              onPress={deinitialize}
+            />
+            </>
+          )}
+
+            {!isInitialized && (
+            <>
+            <Text>Not initialized</Text>
+            <Button
+              title="Initialize"
+              onPress={initialize}
+            />
+            </>
+          )}
 
         </Group>
 
         <Group name="Recording">
 
-        {(!ExpoTmdemo.isRecordingInProgress()) &&
-          <Button
-            title="Start recording"
-            onPress={async () => {
-              await ExpoTmdemo.startRecording();
-            }}
-          />
-        }
+        {isRecording && (
+            <>
+            <Text>Recording is ON</Text>
+            <Button
+              title="Stop Recording"
+              onPress={stopRecording}
+            />
+            </>
+          )}
 
-        {(ExpoTmdemo.isRecordingInProgress()) &&
-          <Button
-            title="Stop recording"
-            onPress={async () => {
-              await ExpoTmdemo.stopRecording();
-            }}
-          />
-        }
+          {!isRecording && (
+            <>
+            <Text>Recording is OFF</Text>
+            <Button
+              title="Start Recording"
+              onPress={startRecording}
+            />
+            </>
+          )}
+
         </Group>
         
       </ScrollView>
